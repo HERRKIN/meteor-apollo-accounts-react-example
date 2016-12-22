0ed8452e67dd3d2265e0005901127727ced0f12a
Nicolás López <nicolaslopezj@me.com>
2016-12-22 12:24:43 -0300

    usa findone

--- apiServer/server/imports/apollo/resolvers/Query/me.js
+++ apiServer/server/imports/apollo/resolvers/Query/me.js
@@ -4,6 +4,8 @@ export default function (root, args, context) {
   if (!context.userId) {
     throw new Error('Unknown User (not logged in)')
   }
-  // return Meteor.users.findOne(context.userId)
-  return context.user
+  return Meteor.users.findOne(context.userId)
+
+  // no tiene todos las variables del user
+  // return context.user
 }
